024acf3be3a66b134040c62b9652acb7ca059bb5
andrew <andrew@dell.domainname>
2019-11-17 20:01:03 +0300

    Add registrations and inst_to_reg conversions

--- wander/overview.py
+++ wander/overview.py
@@ -19,7 +19,11 @@ bp = Blueprint('overview', __name__)
 @login_required
 def overview():
     revenue_json = revenue_plot()
-    plots = {'revenue': revenue_json}
+    regs_json = regs_plot()
+    inst_to_regs_conv_json = inst_to_regs_conv_plot()
+    plots = {'revenue': revenue_json,
+             'regs': regs_json,
+             'inst_to_regs_conv': inst_to_regs_conv_json}
     return render_template('overview/overview.html',
                            title='Overview',
                            plots=plots)
@@ -35,9 +39,30 @@ def revenue_plot():
     fig = px.line(example_data.revenue_df, x="date", y="revenue",
                   title="Revenue",
                   labels=dict(date="Date", revenue="Revenue, $"))
+    fig.update_layout(yaxis=dict(scaleanchor="x", scaleratio=0.8))
+    #fig.update_layout(width=800)
     return fig.to_json()
 
 
+@bp.route('/overview/regs', methods=['POST'])
+@login_required
+def regs_plot():
+    fig = px.line(example_data.regs_df, x="date", y="regs",
+                  title="Registrations",
+                  labels=dict(date="Date", regs="Registrations"))
+    return fig.to_json()
+
+
+@bp.route('/overview/inst_to_regs_conv', methods=['POST'])
+@login_required
+def inst_to_regs_conv_plot():
+    fig = px.line(example_data.inst_to_regs_conv_df,
+                  x="date", y="inst_to_regs_conv",
+                  title="Installs to Regs Conversion",
+                  labels=dict(date="Date",
+                              inst_to_regs_conv="Conversion, %"))
+    fig.update_layout(yaxis=dict(tickformat=',.0%',))
+    return fig.to_json()
 
 
 
