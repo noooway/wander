5f012160c4a2fdd9c042c3ef91b3ff8aac37d3a0
andrew <andrew@dell.domainname>
2019-11-20 23:16:32 +0300

    Separated Data Sources

--- wander/overview.py
+++ wander/overview.py
@@ -1,5 +1,6 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template,
+    request, url_for, current_app
 )
 from werkzeug.exceptions import abort
 from wander.auth import login_required
@@ -10,8 +11,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
 
-# import when project is started with 'flask run' from top-level dir
-from example_data import example_data
 
 bp = Blueprint('overview', __name__)
 
@@ -47,7 +46,7 @@ def revenue_plot():
     default_color = 'orange'
     count = request.form.get('count', default_count)
     col = request.form.get('color', default_color)
-    fig = px.line(example_data.revenue_df, x="date", y="revenue",
+    fig = px.line(current_app.data_sources['revenue'], x="date", y="revenue",
                   title="Revenue",
                   labels=dict(date="Date", revenue="Revenue, $"))
     fig.update_layout(yaxis=dict(scaleanchor="x", scaleratio=0.8))
@@ -59,7 +58,7 @@ def revenue_plot():
 @bp.route('/overview/regs', methods=['POST'])
 @login_required
 def regs_plot():
-    fig = px.line(example_data.regs_df, x="date", y="regs",
+    fig = px.line(current_app.data_sources['regs'], x="date", y="regs",
                   title="Registrations",
                   labels=dict(date="Date", regs="Registrations"))
     return fig.to_json()
@@ -68,7 +67,7 @@ def regs_plot():
 @bp.route('/overview/inst_to_regs_conv', methods=['POST'])
 @login_required
 def inst_to_regs_conv_plot():
-    fig = px.line(example_data.inst_to_regs_conv_df,
+    fig = px.line(current_app.data_sources['inst_to_regs_conv'],
                   x="date", y="inst_to_regs_conv",
                   title="Installs to Regs Conversion",
                   labels=dict(date="Date",
