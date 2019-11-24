8142cd1b476c321d2e74645f236a8ae18712cd94
andrew <andrew@dell.domainname>
2019-11-24 12:01:22 +0300

    Basic retention view: Revenue by Cohorts

--- wander/__init__.py
+++ wander/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from . import db
 from . import auth
 from . import overview
+from . import retention
 
 # works when project is started with 'flask run' from top-level dir
 import data_sources_example
@@ -29,6 +30,7 @@ def create_app(test_config=None):
     db.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(overview.bp)
+    app.register_blueprint(retention.bp)
     app.add_url_rule('/', endpoint='overview.overview')
 
     data_sources_example.init_data_sources(app)
