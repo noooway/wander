3b0af21b4416ee585fe453264f84d6c5989569df
andrew <andrew@dell.domainname>
2019-12-04 07:16:54 +0300

    WIP on update on given time

--- data_sources_example/__init__.py
+++ data_sources_example/__init__.py
@@ -2,6 +2,7 @@ import json
 import sqlite3
 import threading
 import time
+import datetime
 import pandas as pd
 from . import example_data
 
@@ -67,7 +68,7 @@ def init_datasource_with_update(app):
     #todo: pass app or app.data_sources?
     #or pointer to element in app.data_sources?
     update_thread = threading.Thread(target=update_datasource,
-                              args=(app.data_sources, data_lock))
+                                     args=(app.data_sources, data_lock))
     # don't care much about proper exit for now;
     # don't forget to put connection timeout
     #todo: create table; drop on termination
@@ -78,26 +79,61 @@ def init_datasource_with_update(app):
 
 def update_datasource(data_sources, lock):
     db_path = './data_sources_example/regs_pur.db'
+    del_table_sql = 'DROP TABLE IF EXISTS updated;'
+    create_sql = 'CREATE TABLE updated (vals REAL);'
     update_sql = 'INSERT INTO updated VALUES (?);'
     select_sql = 'SELECT * FROM updated;'
-    update_approx_every = 5.0
-    counter = 1
+    #
+    db_conn = sqlite3.connect(
+        db_path,
+        detect_types=sqlite3.PARSE_DECLTYPES
+    )
+    cur = db_conn.cursor()
+    cur.execute(del_table_sql)
+    cur.execute(create_sql)
+    db_conn.commit()
+    #
+    run_approx_every = 15.0
+    counter = 5
+    update_at_minutes = [1, 3]
+    n_of_retries = 5
+    retry_delay = 15
+    wake_every = 30
     while True:
-        start_time = time.time()
-        # todo: check conn successfull
-        # todo: put connection timeout
-        db_conn = sqlite3.connect(
-            db_path,
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        cur = db_conn.cursor()
-        for n in range(5):
-            cur.execute(update_sql, [n * counter])
-        db_conn.commit()
-        with lock:
-            data_sources['updated'] = pd.read_sql_query(select_sql, db_conn)
-            print(data_sources['updated'])
-        db_conn.close()
-        counter = counter + 1
-        time.sleep(update_approx_every -
-                   ((time.time() - start_time) % update_approx_every))
+        current_minute = datetime.datetime.now().minute
+        if current_minute in update_at_minutes and not already_updated:
+            status = None
+            for n in range(n_of_retries):
+                # todo: check conn successfull
+                # todo: put connection timeout
+                status = False
+                db_conn = sqlite3.connect(
+                    db_path,
+                    detect_types=sqlite3.PARSE_DECLTYPES
+                )
+                cur = db_conn.cursor()
+                for n in range(5):
+                    cur.execute(update_sql, [n * counter])
+                    db_conn.commit()
+                with lock:
+                    data_sources['updated'] = pd.read_sql_query(select_sql, db_conn)
+                    print(data_sources['updated'])
+                    status = True
+                db_conn.close()
+                counter = counter + 1
+                #
+                if status:
+                    already_updated = True
+                    break
+                else:
+                    sleep(retry_delay)
+        else:
+            already_updated = False
+            sleep(wake_every)
+    db_conn = sqlite3.connect(
+        db_path,
+        detect_types=sqlite3.PARSE_DECLTYPES
+    )
+    cur = db_conn.cursor()
+    cur.execute(del_table_sql)
+    db_conn.commit()
